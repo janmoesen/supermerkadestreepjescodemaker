f03d331b23e6521bb14fdb8d96cbbbfa91e9d5d1
Jan Moesen <github.com-smk@moesen.nu>
2024-04-12 12:39:31 +0200

    Massage the raw data into something more usable

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 	const btnGenerate = document.querySelector('input[type="button"]');
 	const errorsContainer = document.getElementById('errors');
 	const rawDataContainer = document.getElementById('rawData');
-	const barcodesContainer = document.getElementById('barcodes');
+	const labelsContainer = document.getElementById('labels');
 
 	/* Handle file input. */
 	fileInput.oninput = event => {
@@ -44,9 +44,57 @@
 					rawDataContainer.firstChild.append(pre);
 				}
 
-				/* Process the raw data. */
-				barcodesContainer.textContent = '';
+				/* Turn the raw data into printable labels. */
+				labelsContainer.textContent = '';
 				results.data.forEach(record => {
+					let [description, barcode, sku, socialPrice, regularPrice] = record;
+
+					/* The last record is empty when the file (properly) ends with a
+					 * newline, so skip that. */
+					if (typeof barcode === 'undefined') {
+						return;
+					}
+
+					/* Make sure there is at least 1 digit before the decimal comma.
+					 * `0,90` gets exported as `,9`. */
+					if (socialPrice.match(/^,/)) {
+						socialPrice = `0${socialPrice}`;
+					}
+
+					if (regularPrice.match(/^,/)) {
+						regularPrice = `0${regularPrice}`;
+					}
+
+					/* Re-add the `,00` for integer prices. */
+					if (!socialPrice.match(/,/)) {
+						socialPrice = `${socialPrice},00`;
+					}
+
+					if (!regularPrice.match(/,/)) {
+						regularPrice = `${regularPrice},00`;
+					}
+
+					/* Make sure there are two digits after the decimal comma. */
+					if (!socialPrice.match(/,\d\d$/)) {
+						socialPrice = `${socialPrice}00`.replace(/(,..).*/, '$1');
+					}
+
+					if (!regularPrice.match(/,\d\d$/)) {
+						regularPrice = `${regularPrice}00`.replace(/(,..).*/, '$1');
+					}
+
+					/* Only keep the first barcode. The field seems to contain the
+					 * 5-digit SKU as well, and lots of `ESC` control codes. */
+					barcode = barcode.split(/[\n\u001D]/)[0];
+
+					const labelContainer = document.createElement('div');
+					labelContainer.classList.add('label');
+					const pre = document.createElement('pre');
+					pre.textContent = JSON.stringify({description, barcode, sku, socialPrice, regularPrice}, null, ' ');
+					labelContainer.append(pre);
+
+					labelsContainer.append(labelContainer);
+
 					// JSBarcode it
 					//const svg = document.createElement('svg');
 				});
